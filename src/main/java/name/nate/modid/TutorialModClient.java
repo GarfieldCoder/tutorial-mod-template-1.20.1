742295c3c0c47eb3c9b98f1008308952505a64fa
GarfieldCoder <wolf55bear@gmail.com>
2024-06-01 18:55:39 -0500

    VERY IMPORTANT

---
+++ src/main/java/name/nate/modid/TutorialModClient.java
@@ -0,0 +1,10 @@
+package name.nate.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
